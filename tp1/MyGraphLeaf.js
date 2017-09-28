deecf64861472c53e24f20f2449221ac27a607dc
MeBeeDaisy <up201505505@fe.up.pt>
2017-09-28 09:47:53 +0100

    first commit

---
+++ tp1/MyGraphLeaf.js
@@ -0,0 +1,10 @@
+/**
+ * MyGraphLeaf class, representing a leaf in the scene graph.
+ * @constructor
+**/
+
+function MyGraphLeaf(graph, xmlelem) {
+    this.graph = graph;
+    this.xmlelem = xmlelem;
+}
+
