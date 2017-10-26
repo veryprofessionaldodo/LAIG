e29f48e3ed333fc3271e216a34d275bb122fd985
MeBeeDaisy <up201505505@fe.up.pt>
2017-10-26 09:09:16 +0100

    aniamtion classes

---
+++ tp1/BezierAnimation.js
@@ -0,0 +1,7 @@
+function BezierAnimation(scene){
+	Animation.call(this);
+
+}
+
+BezierAnimation.prototype = Object.create(Animation.prototype);
+BezierAnimation.prototype.constructor = BezierAnimation;
