caefac9a905c34d7898066168c5d998447048ccd
MeBeeDaisy <up201505505@fe.up.pt>
2017-12-24 16:09:22 +0000

    fixed bug

--- Trabalho3/tp1/CameraAnimation.js
+++ Trabalho3/tp1/CameraAnimation.js
@@ -8,7 +8,7 @@ function CameraAnimation(scene, type, ori, dest){
 	this.destPos = this.dest.position;
 
 	if(this.type === 0){
-		this.timeTotal = 1000;
+		this.timeTotal = 10;
 		this.positionDist = vec3.create();
 		vec3.subtract(this.positionDist, this.destPos, this.originPos);
 
@@ -49,7 +49,10 @@ CameraAnimation.prototype.constructor = CameraAnimation;
 CameraAnimation.prototype.stillNotOver = function() {
 	return (Math.abs(this.travelledDisPos[0]) < Math.abs(this.positionDist[0]) || 
 			Math.abs(this.travelledDisPos[1]) < Math.abs(this.positionDist[1]) || 
-			Math.abs(this.travelledDisPos[2]) < Math.abs(this.positionDist[2]))
+			Math.abs(this.travelledDisPos[2]) < Math.abs(this.positionDist[2]) || 
+			Math.abs(this.travelledDisTar[0]) < Math.abs(this.targetDist[0]) || 
+			Math.abs(this.travelledDisTar[1]) < Math.abs(this.targetDist[1]) || 
+			Math.abs(this.travelledDisTar[2]) < Math.abs(this.targetDist[2]))
 }
 
 CameraAnimation.prototype.update = function(deltaTime){
@@ -67,6 +70,9 @@ CameraAnimation.prototype.updateFirstType = function(deltaTime){
 		var distX = this.velPosition[0] * deltaTime;
 		var distY = this.velPosition[1] * deltaTime;
 		var distZ = this.velPosition[2] * deltaTime;
+		/*var dists = vec3.create(distX, distY, distZ);
+		this.origin.translate([distX, distY, distZ]);
+		vec3.add(this.travelledDisPos, dists, this.travelledDisPos);*/
 
 		if(Math.abs(this.travelledDisPos[0]) < Math.abs(this.positionDist[0])){
 			this.origin.position[0] += distX;
@@ -79,6 +85,23 @@ CameraAnimation.prototype.updateFirstType = function(deltaTime){
 		if(Math.abs(this.travelledDisPos[2]) < Math.abs(this.positionDist[2])){
 			this.origin.position[2] += distZ;
 			this.travelledDisPos[2] += distZ;
+		}
+
+		distX = this.velTarget[0] * deltaTime;
+		distY = this.velTarget[1] * deltaTime;
+		distZ = this.velTarget[2] * deltaTime;
+
+		if(Math.abs(this.travelledDisTar[0]) < Math.abs(this.targetDist[0])){
+			this.origin.target[0] += distX;
+			this.travelledDisTar[0] += distX;
+		}
+		if(Math.abs(this.travelledDisTar[1]) < Math.abs(this.targetDist[1])){
+			this.origin.target[1] += distY;
+			this.travelledDisTar[1] += distY;
+		}
+		if(Math.abs(this.travelledDisTar[2]) < Math.abs(this.targetDist[2])){
+			this.origin.target[2] += distZ;
+			this.travelledDisTar[2] += distZ;
 		}
 	}
 	else {
